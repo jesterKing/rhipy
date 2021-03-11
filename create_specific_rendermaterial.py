a938d2b98a35074d19a85f18d9931144c8a2bb2a
Nathan Letwory <nathan@mcneel.com>
2021-03-11 22:53:45 +0200

    Add Create RenderMaterial and Assign example.
    Written using Literate Programming.

---
+++ create_specific_rendermaterial.py
@@ -0,0 +1,19 @@
+import System
+import Rhino.Display
+import Rhino.Render
+import scriptcontext
+import random
+# get object selection
+object_selection = [ob for ob in scriptcontext.doc.Objects if ob.IsSelected]
+# create material
+render_material = Rhino.Render.RenderContent.Create(Rhino.Render.RenderMaterial.PaintMaterialGuid, Rhino.Render.RenderContent.ShowContentChooserFlags.None, scriptcontext.doc)
+render_material.BeginChange(Rhino.Render.RenderContent.ChangeContexts.Program)
+render_material.Name = "PYSCRIPTCREATED " + System.Guid.NewGuid().ToString()
+random_color = Rhino.Display.Color4f(random.random(), random.random(), random.random(), 1.0)
+render_material.SetParameter("color", random_color)
+render_material.EndChange()
+# assign
+for ob in object_selection:
+    print("Adding material", render_material.Name, "to", ob)
+    ob.RenderMaterial = render_material
+    ob.CommitChanges()
