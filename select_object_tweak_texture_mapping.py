98a8b62c16a847d87584f2323c760e6c356b2baa
Nathan Letwory <nathan@mcneel.com>
2021-03-30 00:02:08 +0300

    Add example to get and tweak a RenderTexture.

---
+++ select_object_tweak_texture_mapping.py
@@ -0,0 +1,23 @@
+import scriptcontext as sc
+import Rhino
+import Rhino.Render
+obs = [ob for ob in sc.doc.Objects if ob.IsSelected(False)]
+for ob in obs:
+    render_material = ob.RenderMaterial
+    if not render_material:
+        continue
+    mat = render_material.SimulatedMaterial(Rhino.Render.RenderTexture.TextureGeneration.Allow)
+    if mat.IsPhysicallyBased and mat.PhysicallyBased:
+        slot = Rhino.Render.RenderMaterial.StandardChildSlots.PbrBaseColor
+    else:
+        slot = Rhino.Render.RenderMaterial.StandardChildSlots.Diffuse
+    diffuse_texture = render_material.GetTextureFromUsage(slot)
+    context = Rhino.Render.RenderContent.ChangeContexts.UI
+    repeat = diffuse_texture.GetRepeat()
+    print(repeat)
+    repeat *= 2
+    repeat.Z = 0
+    print(repeat)
+    diffuse_texture.BeginChange(context)
+    diffuse_texture.SetRepeat(repeat, context)
+    diffuse_texture.EndChange()
